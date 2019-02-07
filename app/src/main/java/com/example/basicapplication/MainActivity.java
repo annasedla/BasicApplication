fbf5257c413f2276a77686e87fdd78668464b5fc
Anna Sedlackova <axs1202@case.edu>
2019-02-07 15:04:29 -0500

    Adding buttons and clock.

--- app/src/main/java/com/example/basicapplication/MainActivity.java
+++ app/src/main/java/com/example/basicapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.basicapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Go to broadcast
+    public void goToBroadcast (View view){
+        Intent intent = new Intent (this, BroadcastActivity.class);
+        startActivity(intent);
     }
 }
